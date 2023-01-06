558293b0eb9f0ce96334b215746bd7ced209afa7
Hyejin Eom <hjeom.dev@gmail.com>
2023-01-06 21:40:48 +0900

    add: 멤버서비스 테스트 작성

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,66 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        Member foundMember = memberService.findOne(savedId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(foundMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("hello");
+
+        Member member2 = new Member();
+        member2.setName("hello");
+
+        // when & then
+        memberService.join(member1);
+
+        /* version 1 */
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        /* version 2 */
+        /*
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        */
+
+        /* version 3 */
+        /*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+        */
+    }
+}
