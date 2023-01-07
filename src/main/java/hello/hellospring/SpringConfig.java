5fea6026acefb37759af22b5faea758ac6413155
Hyejin Eom <hjeom.dev@gmail.com>
2023-01-07 13:27:41 +0900

    fix: 멤버레포지토리 클래스가 잘못 설정되어서 수정함.

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memoryMemberRepository());
+        return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memoryMemberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 }
